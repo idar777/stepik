6f88a684e9c53dd2425fb8bb6a8cf6258e0b673a
Idar777 <aydar.abdulkhakov@gmail.com>
2020-03-01 23:44:40 +0300

    second ex4

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
 sudo cp /home/box/web/hello.py /home/box/
 sudo gunicorn -b 0.0.0.0:8080 hello:application &
+sudo /etc/init.d/nginx restart
