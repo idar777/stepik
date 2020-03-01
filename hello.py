725c9e9acc28a01aa316027943a81ff1cd41c53e
Idar777 <aydar.abdulkhakov@gmail.com>
2020-03-01 23:40:22 +0300

    second ex3

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-def hello(environ, start_response): # бизнес-логика
+def application(environ, start_response): # бизнес-логика
    status = '200 OK'
    headers = [('Content-Type', 'text/plain') ]
-   getParam = environ['QUERY_STRING'].replace('&', '\n')
    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
    start_response(status, headers)
    return body
